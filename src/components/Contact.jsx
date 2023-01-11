b59364a9ed6b368178c263df0735fc5ae922be23
Ibimina <ibimina.c.hart@gmail.com>
2023-01-11 12:41:21 +0100

    docs:added jsx content to components and assign id

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React from "react";
 
 export default function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <section id="contact" className="padding">
+      <h2>Contact</h2>
+      <p>
+        {" "}
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem dolor
+        assumenda esse harum fuga commodi quis iusto inventore. Modi, magnam
+        repellendus? Neque explicabo, quaerat perspiciatis eius voluptas ea
+        nostrum enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Rem dolor assumenda esse harum fuga commodi quis iusto inventore. Modi,
+        magnam repellendus? Neque explicabo, quaerat perspiciatis eius voluptas
+        ea nostrum enim.
+      </p>
+      <p>
+        {" "}
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem dolor
+        assumenda esse harum fuga commodi quis iusto inventore. Modi, magnam
+        repellendus? Neque explicabo, quaerat perspiciatis eius voluptas ea
+        nostrum enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Rem dolor assumenda esse harum fuga commodi quis iusto inventore. Modi,
+        magnam repellendus? Neque explicabo, quaerat perspiciatis eius voluptas
+        ea nostrum enim.
+      </p>
+      <p>
+        {" "}
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem dolor
+        assumenda esse harum fuga commodi quis iusto inventore. Modi, magnam
+        repellendus? Neque explicabo, quaerat perspiciatis eius voluptas ea
+        nostrum enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Rem dolor assumenda esse harum fuga commodi quis iusto inventore. Modi,
+        magnam repellendus? Neque explicabo, quaerat perspiciatis eius voluptas
+        ea nostrum enim.
+      </p>
+    </section>
+  );
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React from "react";
 
 export default function Skills() {
   return (
-    <div>Skills</div>
-  )
+    <section id="skills" className="padding">
+      <h2>Skills</h2>
+      <p>
+        {" "}
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem dolor
+        assumenda esse harum fuga commodi quis iusto inventore. Modi, magnam
+        repellendus? Neque explicabo, quaerat perspiciatis eius voluptas ea
+        nostrum enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Rem dolor assumenda esse harum fuga commodi quis iusto inventore. Modi,
+        magnam repellendus? Neque explicabo, quaerat perspiciatis eius voluptas
+        ea nostrum enim.
+      </p>
+      <p>
+        {" "}
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem dolor
+        assumenda esse harum fuga commodi quis iusto inventore. Modi, magnam
+        repellendus? Neque explicabo, quaerat perspiciatis eius voluptas ea
+        nostrum enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Rem dolor assumenda esse harum fuga commodi quis iusto inventore. Modi,
+        magnam repellendus? Neque explicabo, quaerat perspiciatis eius voluptas
+        ea nostrum enim.
+      </p>
+      <p>
+        {" "}
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem dolor
+        assumenda esse harum fuga commodi quis iusto inventore. Modi, magnam
+        repellendus? Neque explicabo, quaerat perspiciatis eius voluptas ea
+        nostrum enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Rem dolor assumenda esse harum fuga commodi quis iusto inventore. Modi,
+        magnam repellendus? Neque explicabo, quaerat perspiciatis eius voluptas
+        ea nostrum enim.
+      </p>
+    </section>
+  );
 }
